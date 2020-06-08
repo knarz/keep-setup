4bd67fbda0b8e25660d3a3aa4ec28e19108f396c
Sascha Hanse <shanse@posteo.de>
2020-06-09 00:33:17 +0200

    add depositor for bonding

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a collection of scripts to:
 2. Request testnet Ether: `$ INFURA_API=APIKEYGOESHERE node faucet.js`
 3. Request a keep grant: `$ node keep_grant.js`
 4. Delegate stake to the fresh wallet and authorize the operator and sortition pools: `$ INFURA_API=APIKEYGOESHERE node access.js PASSWORDGOESHERE`
+5. Send some (hardcoded to 10 right now) eth for bonding: `$ INFURA_API=APIKEYGOESHERE node deposit_eth.js PASSWORDGOESHERE`
 
 These scripts are still somewhat brittle and require an API key for infura.io.
 The scripts have the contract addresses hardcoded and are only meant to be used
---
+++ deposit_eth.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const ethers = require('ethers');
+
+const KeepBonding = require("@keep-network/keep-ecdsa/artifacts/KeepBonding.json")
+
+if (process.argv.length < 3 || !process.argv[2]) {
+	console.error('node access.js [password]');
+	process.exit(1);
+}
+
+async function main() {
+	let wallet
+	try {
+		const j = fs.readFileSync('wallet.json', 'utf8');
+		const w  = await new ethers.Wallet.fromEncryptedJson(j, process.argv[2]);
+		const ip = new ethers.providers.InfuraProvider('ropsten', process.env.INFURA_API);
+		wallet = w.connect(ip);
+
+		const keepBondingContract = new ethers.Contract("0x4368F92Db6d4CA6Dc029D5F1F902137D9b91297B", KeepBonding.abi, wallet);
+
+
+		const deposit = await keepBondingContract.deposit(w.address, {value: ethers.utils.parseEther('10.0')})
+		console.log(`depositing eth`)
+		await deposit.wait()
+
+	} catch(err) {
+		console.error(`Could not authorize: ${err.message}`)
+		process.exit(1)
+	}
+}
+
+main().catch(err => {
+	console.error(err);
+})
+
